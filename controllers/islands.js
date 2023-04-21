550a4e8da16c0bb334358af9a1f31009f9c80e35
Surya-Mohan-Jagani <suryamohan.jagani@gmail.com>
2023-04-20 22:52:12 -0500

    done

--- controllers/islands.js
+++ controllers/islands.js
@@ -16,6 +16,17 @@ exports.islands_list = async function(req, res) {
 exports.islands_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: islands detail: ' + req.params.id);
 };
+// for a specific Costume.
+exports.islands_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await islands.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
 // Handle Costume create on POST.
 exports.islands_create_post = async function(req, res) {
     console.log(req.body)
@@ -45,6 +56,26 @@ res.send('NOT IMPLEMENTED: islands delete DELETE ' + req.params.id);
 exports.islands_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: islands update PUT' + req.params.id);
 };
+// Handle Costume update form on PUT.
+exports.islands_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await islands.findById( req.params.id)
+// Do updates of properties
+if(req.body.island_name)
+toUpdate.island_name = req.body.island_name;
+if(req.body.island_location) toUpdate.island_location = req.body.island_location;
+if(req.body.number_of_visitors) toUpdate.number_of_visitors = req.body.number_of_visitors;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
 // VIEWS
 // Handle a show all view
 exports.islands_view_all_Page = async function(req, res) {
@@ -55,5 +86,75 @@ exports.islands_view_all_Page = async function(req, res) {
     catch(err){
     res.status(500);
     res.send(`{"error": ${err}}`);
+    }
+    };
+
+
+    // Handle Costume delete on DELETE.
+exports.islands_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await islands.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+
+    // Handle a show one view with id specified by query
+exports.islands_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await islands.findById( req.query.id)
+    res.render('islandsdetail',
+    { title: 'islands Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+ // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.islands_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('islandscreate', { title: 'islands Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.islands_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+let result = await islands.findById(req.query.id)
+res.render('islandsupdate', { title: 'islands Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+
+// Handle a delete one view with id from query
+exports.islands_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await islands.findById(req.query.id)
+    res.render('islandsdelete', { title: 'islands Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
     }
     };
--- routes/islands.js
+++ routes/islands.js
@@ -4,4 +4,17 @@ var router = express.Router();
 
 /* GET costumes */
 router.get('/', islands_controlers.islands_view_all_Page );
+
+/* GET detail costume page */
+router.get('/detail', islands_controlers.islands_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', islands_controlers.islands_create_Page);
+
+/* GET create update page */
+router.get('/update', islands_controlers.islands_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', islands_controlers.islands_delete_Page);
+
 module.exports = router;
