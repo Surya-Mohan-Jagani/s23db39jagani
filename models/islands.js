2d9f4d152bdd8fbf86209c8e74a3a37af17abd06
Surya-Mohan-Jagani <suryamohan.jagani@gmail.com>
2023-04-27 22:22:26 -0500

    done

--- models/islands.js
+++ models/islands.js
@@ -1,7 +1,10 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const islandsSchema = mongoose.Schema({
-island_name: String,
-island_location: String,
-number_of_visitors: Number
-})
-module.exports = mongoose.model("islands",islandsSchema)
+  island_name:  {type:String , required: true, minLength:[1,'itemname']},
+  island_location: {type:String,required: true,maxLength:[30]},
+  number_of_visitors: { type: Number, required: true,min: 0, max: 6000},
+   
+});
+
+module.exports = mongoose.model("islands", islandsSchema);
--- routes/islands.js
+++ routes/islands.js
@@ -2,6 +2,28 @@ var express = require('express');
 const islands_controlers= require('../controllers/islands');
 var router = express.Router();
 
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
+
+
+//GET request for one islands.
+router.get('/', islands_controlers.islands_view_all_Page);
+router.get('/detail',secured, islands_controlers.islands_view_one_Page);
+router.get('/create', secured,islands_controlers.islands_create_Page);
+
+router.get('/delete',secured,islands_controlers.islands_delete_Page);
+
+router.get('/update', securedislands_controlers.islands_update_Page);
+
 /* GET costumes */
 router.get('/', islands_controlers.islands_view_all_Page );
 
